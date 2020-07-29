0e54585e4e19c075ec233ac71eec33411d53fb69
NikoronBR <68078528+NikoronBR@users.noreply.github.com>
2020-07-29 15:14:42 -0300

    Animation Tree IDLE

--- O Jogo/Player/Player.gd
+++ O Jogo/Player/Player.gd
@@ -1,63 +1,66 @@
 extends KinematicBody2D
 
-export var VELOCIDADE_MAX = 100
-export var ACELERACAO = 600
-export var FREIO = 600
+export var MAX_SPEED = 100
+export var ACCELERATION = 600
+export var FRICTION = 600
 
 enum {
-	ANDAR,
-	ATACAR
+	MOVE,
+	ATTACK
 }
 
 #GustavoEsteveAqui
 
-var velocidade = Vector2.ZERO
-var state = ANDAR
+var velocity = Vector2.ZERO
+var state = MOVE
 
-#onready var animationPlayer = $AnimationPlayer
-#onready var animationTree = $AnimationTree
-#onready var animationState = animationTree.get("parameters/playback")
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 #onready var hurtbox = $Hurtbox
+
+func _ready():
+	animationTree.active = true
 
 func _physics_process(delta):
 	match state:
-		ANDAR:
-			andar_state(delta)
+		MOVE:
+			move_state(delta)
 		
-		#ATACAR:
-			#atacar_state()
+		#ATTACK:
+			#attack_state()
 			
-func andar_state(delta):
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		#animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Idle/blend_position", input_vector)
 		#animationTree.set("parameters/Run/blend_position", input_vector)
 		#animationTree.set("parameters/Attack/blend_position", input_vector)
-		#animationState.travel("Run")
-		velocidade = velocidade.move_toward(input_vector * VELOCIDADE_MAX, ACELERACAO * delta)
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		
 	else:
-		#animationState.travel("Idle")
-		velocidade = velocidade.move_toward(Vector2.ZERO, FREIO * delta)
+		animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	move()
 	
 	#if Input.is_action_just_pressed("ataque"):
-		#state = ATACAR
+		#state = ATTACK
 
-#func atacar_state():
-	#velocidade = Vector2.ZERO
+#func attack_state():
+	#velocity = Vector2.ZERO
 	#animationState.travel("Attack")
 
 func move():
-	velocidade = move_and_slide(velocidade)
+	velocity = move_and_slide(velocity)
 
-#func atacar_fim():
-	#state = ANDAR
+#func atacck_fim():
+	#state = MOVE
 
 #func _on_Hurtbox_area_entered(_area):
 	#stats.health -= 1
