808dc0b7d45532a20510042235d3373cbbb56a05
GuerreiroG <guto_guerreiro@hotmail.com>
2020-07-31 00:43:03 -0300

    Conserto de pequenos erros e comentários.
    As árvores estavam fora do y-sort por isso não estavam interagindo corretamento com o jogador, foi corrigido.
    Pedra grande sem colisão foi removida.
    Adicionados comentários ao script do jogador.

--- O Jogo/Player/Player.gd
+++ O Jogo/Player/Player.gd
@@ -1,28 +1,32 @@
 extends KinematicBody2D
 
+#Configurando valores de velocidade máxima, aceleração e friccção do personagem.
 export var MAX_SPEED = 100
 export var ACCELERATION = 600
 export var FRICTION = 600
 
+#Ligando as ações do personagem a números.
 enum {
 	MOVE,
 	ATTACK
 }
 
-#GustavoEsteveAqui
-
+#Definindo variáveis para velocidade e estado do personagem.
 var velocity = Vector2.ZERO
 var state = MOVE
 
+#Definindo variaveis para a animação.
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 #onready var hurtbox = $Hurtbox
 
 func _ready():
+	#Ativando a árvore de animação quando o jogo abrir.
 	animationTree.active = true
 
 func _physics_process(delta):
+	#Defininfo a animação e ações do personagem baseados nos inputs
 	match state:
 		MOVE:
 			move_state(delta)
@@ -31,6 +35,7 @@ func _physics_process(delta):
 			#attack_state()
 			
 func move_state(delta):
+	#Ligando inputs do teclado a direções que o jogador irá se movimentar. 
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
