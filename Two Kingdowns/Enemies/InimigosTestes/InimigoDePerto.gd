fd93b3f0ea8182b27d5267b4b89bef76d49a705e
NikoronBR <68078528+NikoronBR@users.noreply.github.com>
2020-10-15 15:11:57 -0300

    Removido Wander do Inimigo de Perto.

--- Two Kingdowns/Enemies/InimigosTestes/InimigoDePerto.gd
+++ Two Kingdowns/Enemies/InimigosTestes/InimigoDePerto.gd
@@ -5,12 +5,10 @@ var FantasmaDeathEffect = preload("res://Effects/FantasmaDeathEffect.tscn")
 export var ACCELERATION = 385
 export var MAX_SPEED = 50
 export var FRICTION = 185
-export var WANDER_TARGET_RANGE = 4
 
 enum {
 	IDLE,
-	WANDER,
-	CHASE
+	CHASE,
 }
 
 var state = CHASE
@@ -22,10 +20,10 @@ onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftColision
-onready var wanderController = $WanderController
+
 
 func _ready():
-	state = pick_random_state([IDLE, WANDER])
+	state = IDLE
 
 func _physics_process(delta):
 # Cria uma fricção para o inimigo resistir ao knockback
@@ -37,23 +35,6 @@ func _physics_process(delta):
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
 			
-			#chama o 'update_wander' quando intervalo acaba
-			if wanderController.get_time_left() == 0:
-				update_wander()
-				
-		WANDER:
-			seek_player()
-			
-			#move o inimigo na posição alvo gerada no WanderController
-			accelerate_towards_point(wanderController.target_position, delta)
-			
-			#If's que chama o update quando o intervalo acaba ou o inimigo chega na posição.
-			if wanderController.get_time_left() == 0:
-				update_wander()
-		
-			if global_position.distance_to(wanderController.target_position) <= WANDER_TARGET_RANGE:
-				update_wander()
-				
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
@@ -75,17 +56,6 @@ func accelerate_towards_point(point, delta):
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
-
-#função para anternar entre states IDLE e WANDER aleatóriamente e reniciar
-#o intervalo do WanderController.
-func update_wander():
-	state = pick_random_state([IDLE, WANDER])
-	wanderController.start_wander_timer(rand_range(1, 3))
-
-#função para gerar váriavel IDLE e WANDER de forma aleatória
-func pick_random_state(state_list):
-	state_list.shuffle()
-	return state_list.pop_front()
 
 # Quando detecta um Ataque diminui a vida e aplica a direção e velocidade do knockback
 func _on_Hurtbox_area_entered(area):
