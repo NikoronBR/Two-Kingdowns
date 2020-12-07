d679aea7315e354c0322eb937cbbb828377e7822
Lucas Vargas <68078528+NikoronBR@users.noreply.github.com>
2020-12-07 15:54:12 -0300

    Alterado botão para "tentar de novo"

--- Two Kingdowns/UI/DeathMenu/DeathMenu.gd
+++ Two Kingdowns/UI/DeathMenu/DeathMenu.gd
@@ -2,17 +2,17 @@ extends Control
 
 #faz com que o menu de morte fique escondido
 func _ready():
-	set_process(false)
+	set_process_unhandled_key_input(false)
 	hide()
 	
 #Quando o player morre mostra o menu e ativa o "process" do node	
 func _on_Player_player_morto(_global_position):
+	set_process_unhandled_key_input(true)
 	show()
-	set_process(true)
 
-#Checa se a ação "ui_select" (atualmente condiz ao espaço) foi ativado,
+#Checa se a ação algum botão foi pressionado,
 #então repõe a vida do player e recarrega a cena.
-func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_select"):
+func _unhandled_key_input(event):
+	if event.is_pressed():
 		PlayerStats.health = PlayerStats.max_health
 		get_tree().reload_current_scene()
